e16ce71685bc4b7a5ca2ecd4a10e0635f7375a10
honeion <honeion2012@gmail.com>
2023-05-12 01:39:52 +0900

    Implement #4 - 요청에 따른 응답 추가
    Admin Controller 부분 adminPlaces 메소드 ModelAndView로 반환하여 Thymeleaf 방식으로 출력

--- getinline/src/main/java/com/example/getinline/controller/AdminController.java
+++ getinline/src/main/java/com/example/getinline/controller/AdminController.java
@@ -1,9 +1,14 @@
 package com.example.getinline.controller;
 
+import com.example.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //뷰들 admin으로 시작하는 Convention을 위해
 //클래스 레벨로 매핑하고 그밑에 연결되는 식
@@ -12,8 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
+//    @GetMapping("/places")
+//    public String adminPlaces() { return "admin/places"; }
     @GetMapping("/places")
-    public String adminPlaces() { return "admin/places"; }
+    public ModelAndView adminPlaces(
+            PlaceType placeType,
+            String placeName,
+            String address
+    ) //@RequestParam = true가 기본인데, 안적으면 기본값이 false 어차피 전부 optional이라
+    {
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("placeType",placeType);
+        map.put("placeName",placeName);
+        map.put("address",address);
+        return new ModelAndView("admin/places",map);
+    }
 
     @GetMapping("/places/{placeId}")
     public String adminPlaceDetail(@PathVariable Integer placeId) { return "admin/place-detail"; }
